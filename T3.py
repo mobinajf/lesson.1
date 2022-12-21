a22471d5df06f4c7a727be233b8264f9d1916625
mobinajf <121153553+mobinajf@users.noreply.github.com>
2022-12-21 11:38:38 +0330

    Add files via upload

--- T3.py
+++ T3.py
@@ -1,11 +1,13 @@
-import random
-
-n=int(input("please enter n:"))
-num=[]
-while True:
-    b=random.randint(0,n)
-    if b not in num:
-        num.append(b)
-    elif len(num)==n:
-        print(num)
-        break
+for i in range(20):
+ S1=float(int(input("average1:")))
+ S2=float(int(input("average2:")))
+ S3=float(int(input("average3:")))
+ name=input("please enter your full name:")
+ mean=(S1+S2+S3)/3
+ if mean>=17:
+    average="greate"
+ if 17>mean>=12:
+    average="normal"
+ elif mean<12:
+    average="fail"
+ print(average)
